d237cf51d465f3ad8d44f04563a3593ce17c92f9
Arthur Vasconcelos <vasconcelos.arthur@gmail.com>
2018-11-29 20:27:48 -0200

    remove lazy-cache

--- utils.js
+++ utils.js
@@ -1,21 +1,31 @@
 'use strict';
 
-var utils = require('lazy-cache')(require);
-var fn = require;
-require = utils;
+// var utils = require('lazy-cache')(require);
+// var fn = require;
+// require = utils;
 
 /**
  * Module dependencies
  */
 
-require('define-property', 'define');
-require('get-value', 'get');
-require('is-valid-app', 'isValid');
-require('isobject', 'isObject');
-require('mixin-deep', 'merge');
-require('option-cache', 'Options');
-require('set-value', 'set');
-require = fn;
+// require('define-property', 'define');
+// require('get-value', 'get');
+// require('is-valid-app', 'isValid');
+// require('isobject', 'isObject');
+// require('mixin-deep', 'merge');
+// require('option-cache', 'Options');
+// require('set-value', 'set');
+// require = fn;
+
+var utils = {
+  define: require('define-property'),
+  get: require('get-value'),
+  isValid: require('is-valid-app'),
+  isObject: require('isobject'),
+  merge: require('mixin-deep'),
+  Options: require('option-cache'),
+  set: require('set-value')
+};
 
 /**
  * Expose `utils` modules
